ca314dfa4c2697eb415aefee8172dc44d44284e3
Serge Nahas <sergenhs@gmail.com>
2016-06-18 19:37:32 -0400

    Remove un-used imports

--- getapet/src/main/java/controller/PetController.java
+++ getapet/src/main/java/controller/PetController.java
@@ -1,15 +1,11 @@
 package controller;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
