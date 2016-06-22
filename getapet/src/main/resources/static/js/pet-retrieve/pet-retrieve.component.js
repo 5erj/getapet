a197fda282adafc196811fa67f1ab6ffcb5a096f
Serge Nahas <sergenhs@gmail.com>
2016-06-22 18:59:45 -0400

    Add support for disowning pets.
    - Add a button on the 'My Pets' tab that allows for the deletion of a
    retrieved Pet.

--- getapet/src/main/resources/static/js/pet-retrieve/pet-retrieve.component.js
+++ getapet/src/main/resources/static/js/pet-retrieve/pet-retrieve.component.js
@@ -6,14 +6,27 @@ angular
 		templateUrl: 'js/pet-retrieve/pet-retrieve.template.html',
 		controller: ['Pet', 
 		    function PetRetrieveController(Pet) {
+				// Flag to control the message that needs to be 
+				// displayed on pet deletion
+				this.deletedPet = false;
+				
 				var self = this;
 				
 				self.retrievePet = function() {
 					return Pet.get({petId: self.petId}, 
 							function(value, responseHeaders) {
 						self.pet = value;
+						self.deletedPet = false;
 					})
 				}
 				
+				self.deletePet = function() {
+					return Pet.delete({petId: self.petId}, 
+							function(value, responseHeaders) {
+						console.log('pet removed');
+						self.pet = null;
+						self.deletedPet = true;
+					})
+				}
 			}]
 	});
