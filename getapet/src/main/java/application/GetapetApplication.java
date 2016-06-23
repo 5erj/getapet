e05795f8adfcb214f98d7dd8b46f01e59e2259db
Serge Nahas <sergenhs@gmail.com>
2016-06-23 07:58:42 -0400

    Switch to 'WAR' packaging from 'JAR'

--- getapet/src/main/java/application/GetapetApplication.java
+++ getapet/src/main/java/application/GetapetApplication.java
@@ -2,6 +2,8 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
@@ -10,7 +12,12 @@ import repository.PetRepository;
 @SpringBootApplication
 @ComponentScan(basePackages={"controller", "service"})
 @EnableMongoRepositories(basePackageClasses=PetRepository.class)
-public class GetapetApplication {
+public class GetapetApplication extends SpringBootServletInitializer{
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GetapetApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(GetapetApplication.class, args);
