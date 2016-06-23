e2bfdb681c7be91f87fa8077bc4d0c66ab7a26ab
Serge Nahas <sergenhs@gmail.com>
2016-06-23 14:35:51 -0400

    Enable navigation to pet lists from carousel

--- getapet/src/main/resources/static/app/carousel/carousel.component.js
+++ getapet/src/main/resources/static/app/carousel/carousel.component.js
@@ -4,7 +4,7 @@ angular
 	.module('carousel')
 	.component('carousel', {
 		templateUrl: 'app/carousel/carousel.template.html',
-		controller: function CarouselController() {
+		controller: function CarouselController($location) {
 			 this.myInterval = 5000;
 			 this.noWrapSlides = false;
 			 this.active = 0;
@@ -12,15 +12,23 @@ angular
 			 
 			 this.slides.push({
 			      image: 'app/img/carousel/dog.png',
+			      link: '/dogs',
 			      id: 0
 			    });
 			 this.slides.push({
 			      image: 'app/img/carousel/cat.png',
+			      link: '/cats',
 			      id: 1
 			    });
 			 this.slides.push({
 			      image: 'app/img/carousel/bird.png',
+			      link: '/birds',
 			      id: 2
 			    });
+			 
+			 var self = this;
+			 this.navigate = function(slideId) {
+				$location.url(self.slides[slideId].link);
+			 }
 		}
 	});
